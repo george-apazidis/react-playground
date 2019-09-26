33a0ae70efd53eece4afb5736ac5e684edbd3ec4
George Apazidis <geoapa@gmail.com>
2019-09-26 09:02:58 -0400

    initial commit

---
+++ src/composition/Split2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Split2() {
+  return (
+    <div className="split">
+      This is the content of the split component. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+    </div>
+  );
+}
+
+export default Split2;
