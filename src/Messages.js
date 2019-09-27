8ccaf4ed79e34bf647b9b02cf6fd33cd90bc7d25
George Apazidis <geoapa@gmail.com>
2019-09-27 10:40:34 -0400

    added more snapshot test cases

--- src/Messages.js
+++ src/Messages.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './Messages.css';
+import React from "react";
+import "./Messages.css";
 
 export default function Messages(props) {
-  const badge = props.unread 
-    ? <div className="unread_count">{props.unread}</div> 
-    : null;
+  const badge = +props.unread ? (
+    <div className="unread_count badge">{props.unread}</div>
+  ) : null;
   return (
     <div className="messages">
-      {props.name} 
+      {props.name}
       {badge}
-    </div>  
+    </div>
   );
 }
--- src/Messages.test.js
+++ src/Messages.test.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Messages from './Messages';
-import renderer from 'react-test-renderer';
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import Messages from "./Messages";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<Messages />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders the UI as expected', () => {
+it("renders the UI as expected", () => {
   const tree = renderer
-    .create(<Messages name="Messages" unread={4}/>)
+    .create(<Messages name="Messages" unread={4} />)
     .toJSON();
-  expect(tree).toMatchSnapshot();  
-  });
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders the UI as expected with no unreads", () => {
+  const tree = renderer
+    .create(<Messages name="Messages" unread={0} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
