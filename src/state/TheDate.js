5451e878164c1cb9768ca451b55d6bf5a1466fc9
George Apazidis <geoapa@gmail.com>
2019-09-27 16:07:23 -0400

    added TheDate component to practice manipulating state

---
+++ src/state/TheDate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TheDate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { datetime: new Date() };
+  }
+  render() {
+    return <div>{this.state.datetime.toLocaleString()}</div>;
+  }
+}
+
+export default TheDate;
